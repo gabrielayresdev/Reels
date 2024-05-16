dabfb159687a51bc636bda7df845aa630e6d457e
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-05-16 20:03:32 -0300

    feat: video repository set to public, so user can request a video without downloading it

--- back/server.ts
+++ back/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import configDotenv from "./src/config/dotenv";
 import router from "./src/routes/routes";
+import * as path from "path";
 // import cors from 'cors';
 // import routes from './src/routes/routes';
 
@@ -9,9 +10,13 @@ configDotenv();
 const app = express();
 const port = process.env.PORT;
 
+// Defina o diretório onde os vídeos estão armazenados
+const videosPath = path.join(__dirname, "/uploads");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
+app.use("/videos", express.static(videosPath));
 app.use(router);
 
 app.get("/", (req, res) => {
