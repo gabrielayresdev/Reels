f70b1b1f0e1dec92e7d597e7835d610096808440
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-05-18 15:43:46 -0300

    feat: checkAdmin and checkAdminOrOwner middleware

--- back/src/middlewares/checkAdminOrOwner.ts
+++ back/src/middlewares/checkAdminOrOwner.ts
@@ -3,7 +3,11 @@ const prisma = new PrismaClient();
 
 import { Request, Response, NextFunction } from "express";
 
-async function adm(req: Request, res: Response, next: NextFunction) {
+async function checkAdminOrOwner(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
     if (!req.user) return res.status(400).json({ message: "Unauthorized" });
 
@@ -12,13 +16,19 @@ async function adm(req: Request, res: Response, next: NextFunction) {
         id: req.user,
       },
     });
+    const { id } = req.params;
+    const target = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-    if (!requester) return res.status(400).send("User not found");
-    if (requester.adm) return next();
+    if (!requester || !target) return res.status(400).send("User not found");
+    if (requester.adm || target.id === requester.id) next();
     else return res.status(401).json({ message: "Unauthorized" });
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
-export { adm };
+export { checkAdminOrOwner };
